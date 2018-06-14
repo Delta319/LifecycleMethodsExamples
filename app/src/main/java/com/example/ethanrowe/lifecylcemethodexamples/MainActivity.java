b27baf4cb60edcf2d7c1b0249959a235e1dfa6ab
Ethan Rowe <redsoxrowe@gmail.com>
2018-06-14 11:01:12 -0400

    Completed project

--- app/src/main/java/com/example/ethanrowe/lifecylcemethodexamples/MainActivity.java
+++ app/src/main/java/com/example/ethanrowe/lifecylcemethodexamples/MainActivity.java
@@ -19,25 +19,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Creates small popup at the bottom of the screen
         Toast.makeText(this, "onCreate", Toast.LENGTH_LONG).show();
 
+        //Links the variable reference to a specific UI element by id (name)
         newActivityButton = findViewById(R.id.new_activity_button);
         finishProgramButton = findViewById(R.id.finish_activity_button);
 
+        //Setting intent to navigate from THIS activity to a different one
         newActivity = new Intent(this, SecondActivity.class);
 
+        //Method to initiate onClickListeners for the buttons in the UI
         setListeners();
     }
 
     private void setListeners() {
+        //Set listener for newActivityButton only
         newActivityButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //Takes our intent and tells it to use that to start a new Activity
                 startActivity(newActivity);
+            }
+        });
+
+        //Sets listener for finishProgramButton only
+        finishProgramButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Closes current activity (in this location this call will close our program)
+                finish();
+
             }
         });
     }
 
+    //Lifecycle methods listed below
     @Override
     protected void onStart() {
         super.onStart();
